24f3063bcf3274c4cda91872059deae6bf814217
Vincent Oppedijk <Vincent.oppedijk@asml.com>
2024-12-02 20:32:00 +0100

    Day 2.

--- src/day02/Part2.java
+++ src/day02/Part2.java
@@ -2,6 +2,7 @@ package day02;
 
 import util.FileProcessor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Part2 {
@@ -9,6 +10,101 @@ public class Part2 {
 
     public static void main(String[] args) {
         Part2 part2 = new Part2();
+        int count = 0;
+        int puzzle2 = 0;
 
+        for (String report : part2.input) {
+            String[] reportStringArr = report.split(" ");
+            int[] reportIntArr = new int[reportStringArr.length];
+
+            for (int i = 0; i < reportStringArr.length; i++) {
+                reportIntArr[i] = Integer.parseInt(reportStringArr[i]);
+            }
+
+            if ((part2.isIncreasing(reportIntArr) || part2.isDecreasing(reportIntArr))
+            && part2.isDifferenceThreeOrLess(reportIntArr)) {
+                count++;
+            } else {
+                if (part2.isValidReportWhenOneDigitRemoved(reportIntArr)) {
+                    puzzle2++;
+                }
+            }
+        }
+
+        System.out.println("part 1: " + count); //606
+        System.out.println(String.format("part 2: count = %d + extra = %d answer= %d", count, puzzle2, count+puzzle2));
+    }
+
+    boolean isValidReportWhenOneDigitRemoved(int[] report) {
+        boolean result = false;
+        int amountOfDigits = report.length;
+        for (int i = 0; i < amountOfDigits; i++) {
+            List<Integer> lst = new ArrayList<>(amountOfDigits);
+            for (int y : report)
+            {
+                lst.add(y);
+            }
+            lst.remove(i);
+
+            int[] arr = lst.stream()
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+
+            if ((isIncreasing(arr) || isDecreasing(arr))
+                    && isDifferenceThreeOrLess(arr)) {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
+
+    public boolean isIncreasing(int[] input) {
+        int nr = input[0];
+        boolean isIncreasing = true;
+
+        for (int i = 1; i < input.length; i++) {
+            if (nr >= input[i]) {
+                isIncreasing = false;
+            }
+            nr = input[i];
+        }
+
+        return isIncreasing;
+    }
+
+    public boolean isDecreasing(int[] input) {
+        int nr = input[0];
+        boolean isDecreasing = true;
+
+        for (int i = 1; i < input.length; i++) {
+            if (nr <= input[i]) {
+                isDecreasing = false;
+            }
+            nr = input[i];
+        }
+
+        return isDecreasing;
+    }
+
+    public boolean isDifferenceThreeOrLess(int[] input) {
+        int nr = input[0];
+        boolean isValid = true;
+
+        for (int i = 1; i < input.length; i++) {
+            if (nr >= input[i]) {
+                if (!(nr - input[i] < 4)) {
+                    isValid = false;
+                }
+            } else {
+                if (!(input[i] - nr < 4)) {
+                    isValid = false;
+                }
+            }
+            nr = input[i];
+        }
+
+        return isValid;
     }
 }
--- src/day02/Part2Test.java
+++ src/day02/Part2Test.java
@@ -1,10 +1,79 @@
 package day02;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Part2Test {
     @Test
-    void test() {
+    void canCheckIfIsIncreasing() {
+        int[] input = {1, 2, 3, 4, 5};
+        Part2 p = new Part2();
 
+        Assertions.assertTrue(p.isIncreasing(input));
     }
+
+    @Test
+    void canCheckIfIsDecreasing() {
+        int[] input = {5, 4, 3, 2, 1};
+        Part2 p = new Part2();
+
+        Assertions.assertTrue(p.isDecreasing(input));
+    }
+
+    @Test
+    void canCheckIfNrDifferenceIsThreeOrLess() {
+        int[] input = {50, 49, 46};
+        Part2 p = new Part2();
+
+        Assertions.assertTrue(p.isDifferenceThreeOrLess(input));
+    }
+
+    @Test
+    void canCheckIfReportIsValidWhenDigitRemoved() {
+        int[] input = {1, 2, 4, 4, 5};
+        Part2 p = new Part2();
+
+        Assertions.assertTrue(p.isValidReportWhenOneDigitRemoved(input));
+    }
+
+    @Test
+    void canCheckIfReportIsValidWhenDigitRemoved2() {
+        int[] input = {1, 2, 4, 9, 5};
+        Part2 p = new Part2();
+
+        Assertions.assertTrue(p.isValidReportWhenOneDigitRemoved(input));
+    }
+
+    @Test
+    void canCheckIfReportIsValidWhenDigitRemoved3() {
+        int[] input = {1, 2, 8, 4, 5};
+        Part2 p = new Part2();
+
+        Assertions.assertTrue(p.isValidReportWhenOneDigitRemoved(input));
+    }
+
+    @Test
+    void canCheckIfReportIsValidWhenDigitRemoved4() {
+        int[] input = {2, 2, 3, 4, 5};
+        Part2 p = new Part2();
+
+        Assertions.assertTrue(p.isValidReportWhenOneDigitRemoved(input));
+    }
+
+    @Test
+    void canCheckIfReportIsValidWhenDigitRemoved5() {
+        int[] input = {1, 2, 3, 4, 2};
+        Part2 p = new Part2();
+
+        Assertions.assertTrue(p.isValidReportWhenOneDigitRemoved(input));
+    }
+
+    @Test
+    void canCheckIfReportIsValidWhenDigitRemoved6() {
+        int[] input = {2, 2, 3, 3, 5};
+        Part2 p = new Part2();
+
+        Assertions.assertFalse(p.isValidReportWhenOneDigitRemoved(input));
+    }
+
 }
